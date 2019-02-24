de087a77bb9c5652947765b0c7be3891a5a79939
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-02-24 12:45:01 +0000

    Added support to cache news for the amount of time configured in the yaml file (in seconds).

--- news.py
+++ news.py
@@ -8,15 +8,19 @@ from workflow import Workflow3
 
 PARSERS_ROOT = 'parsers'
 
+def get_news_from_parser(parser):
+    parser = importlib.import_module('{0}.{1}'.format(PARSERS_ROOT, parser))
+    news = parser.get_news()  # returns a list of dictionaries: [ { 'title': '', 'subtitle': '', 'icon': '' } ]
+    return news
 
 def main(wf):
     news_sources = yaml.safe_load(open("sources.yml"))
 
     for source_name in news_sources:
-        # invoke the parser
-        parser = importlib.import_module('{0}.{1}'.format(PARSERS_ROOT, news_sources[source_name]['parser']))
-        news = parser.get_news()  # returns a list of dictionaries: [ { 'title': '', 'subtitle': '', 'icon': '' } ]
-
+        # retrieve posts from cache if available and no more than cacheForSecs old (usually, 1 minute)
+        # if not, invoke the parser
+        args=[news_sources[source_name]['parser']]
+        news = wf.cached_data(source_name, get_news_from_parser, max_age=news_sources[source_name]['cacheforsecs'], data_func_args=args)
         for n in news:
             wf.add_item(
                 title=n['title'],
