3833c3df9f6ea9ab19a6e1d69d74d41b9f0e3d22
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-02-24 18:44:31 +0000

    Added fuzzy search argument to select news sources.

--- news.py
+++ news.py
@@ -8,25 +8,40 @@ from workflow import Workflow3
 
 PARSERS_ROOT = 'parsers'
 
+
 def get_news_from_parser(parser):
     parser = importlib.import_module('{0}.{1}'.format(PARSERS_ROOT, parser))
     news = parser.get_news()  # returns a list of dictionaries: [ { 'title': '', 'subtitle': '', 'icon': '' } ]
     return news
 
+
 def main(wf):
+    # get query from Alfred
+    source_filter_query = None
+    if len(wf.args):
+        source_filter_query = wf.args[0]
+
     news_sources = yaml.safe_load(open("sources.yml"))
 
-    for source_name in news_sources:
+    filtered_source_names = news_sources.keys()
+
+    if (source_filter_query):
+        # fuzzy filter sources
+        filtered_source_names = wf.filter(source_filter_query, news_sources.keys())
+
+    for source_name in filtered_source_names:
         # retrieve posts from cache if available and no more than cacheForSecs old (usually, 1 minute)
         # if not, invoke the parser
-        args=[news_sources[source_name]['parser']]
-        news = wf.cached_data(source_name, get_news_from_parser, max_age=news_sources[source_name]['cacheforsecs'], data_func_args=args)
+        args = [news_sources[source_name]['parser']]
+        news = wf.cached_data(source_name, get_news_from_parser, max_age=news_sources[source_name]['cacheforsecs'],
+                              data_func_args=args)
+
         for n in news:
             wf.add_item(
                 title=n['title'],
                 subtitle='[{0}] {1}'.format(source_name, n['subtitle']),
                 icon=n['icon'],
-                arg=n['subtitle'], # tell alfred to pass the url to the next action in the workflow
+                arg=n['subtitle'],  # tell alfred to pass the url to the next action in the workflow
                 valid=True
             )
 
