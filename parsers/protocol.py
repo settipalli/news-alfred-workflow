1214910ea383d730e2feb9d72d74f4245df53d4f
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2020-07-27 14:35:03 +0200

    Added support to fetch news links from Protocol.

---
+++ parsers/protocol.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+from bs4 import BeautifulSoup
+from workflow import web
+
+
+def get_data(*args):
+    """
+    Parse the protocol URL
+    :return: {  title = news.text,
+                subtitle= news.hyperlink,
+                icon=ICON_WEB
+             }
+    """
+
+    url = 'https://www.protocol.com/'
+
+    r = web.get(url)
+
+    # throw an error if request failed
+    # Workflow3 will catch this and show it to the user
+    r.raise_for_status()
+
+    # extract news items
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # with open('testdata-hacker-news.html') as f:
+    #     soup = BeautifulSoup(f.read(), 'html.parser')
+
+    result = []
+    for news in soup.find_all("a", {"class": "custom-post-headline"}):
+        result.append({
+            'title': news.text.strip(),
+            'subtitle': news['href'],
+            'link': news['href'],
+        })
+
+    return result
+
+
+if __name__ == u'__main__':
+    pass
